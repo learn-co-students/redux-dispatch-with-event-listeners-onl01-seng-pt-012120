d26704fc0d4d7019b5efa15b141f31d065646fca
LeonorPDX <leonoracolbert@gmail.com>
2020-10-13 12:03:37 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,36 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return { count: state.count + 1 };
+        case 'DECREASE_COUNT':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render()
+}
+
+function render() {
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+let iBtn = document.getElementById('i-button');
+
+iBtn.addEventListener('click', () => {
+    dispatch({ type: 'INCREASE_COUNT' })
+})
+
+let dBtn = document.getElementById('d-button');
+
+dBtn.addEventListener('click', () => {
+    dispatch({ type: 'DECREASE_COUNT' })
+})
+
+dispatch({ type: '@@INIT' })
